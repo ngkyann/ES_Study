0720edf585fa60ddbceab8077828ccbb97ccce18
ngkyann <satthuma1357@gmail.com>
2026-05-23 17:51:07 +0700

    Version 2.2.2 Beta

--- src/lib/constants/var.dart
+++ src/lib/constants/var.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const String geminiApiKey = 'AIzaSyDEvXt5krUQ059RVimI7U6BluQtamVy_Hs';
 ValueNotifier<String> languageNotifier = ValueNotifier<String>("Tiếng Việt");
-String version = " 2.2.0 Beta";
+String version = " 2.2.2 Beta";
