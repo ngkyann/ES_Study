7188a8f6e41d5e40e7f8a8f749e174ce0f979824
ngkyann <satthuma1357@gmail.com>
2026-09-16 12:51:46 +0700

    Remove API_KEY in repo.

--- src/lib/constants/var.dart
+++ src/lib/constants/var.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const String geminiApiKey = 'AIzaSyDEvXt5krUQ059RVimI7U6BluQtamVy_Hs';
+const String geminiApiKey = 'YOUR_API_KEY_HERE';
 ValueNotifier<String> languageNotifier = ValueNotifier<String>("Tiếng Việt");
 String version = " 2.3.0 Beta";
